661e2a905617856d084acf41c8a8a97c5bedc8fc
Aristóbulo Meneses <aristobulo@gmail.com>
2019-09-11 19:20:45 +0200

    First commit: project skel

---
+++ server.py
@@ -0,0 +1,23 @@
+from aiohttp import web
+from django import setup
+from django.conf import settings
+
+from montgomery import settings as my_settings  # not the same as django.conf.settings
+
+
+async def setup_django(app):
+    settings.configure(
+        INSTALLED_APPS=my_settings.INSTALLED_APPS,
+        DATABASES=my_settings.DATABASES)
+    setup()
+
+
+async def main():
+    web_app = web.Application()
+    web_app.on_startup.append(setup_django)
+    return web_app
+
+
+if __name__ == '__main__':
+    app = await main()
+    web.run_app(app)
